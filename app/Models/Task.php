c8c0544e61bd8623323ded8cf4c6d71fdb64438c
Lucas Lessa Pacheco <lucas.pacheco@box360.net.br>
2022-11-21 08:37:12 -0300

    efetuando relacionamento entre tabelas de dados

---
+++ app/Models/Task.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use App\Models\Category;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date',
+        'user_id',
+        'category_id'
+    ];
+    //CONFIGURANDO RELACIONAMENTO DO TASK COM USER PARA RECEBER O ID DO USERS
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    //CONFIGURANDO RELACIONAMENTO DO TASK COM CATEGORY PARA RECEBER O ID DO CATEGORIES
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+}
